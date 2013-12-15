29eb61a587c643d3b9f7b57d15f8130f01e5c270
Alex Burner <alex.burner@gmail.com>
2013-12-14 17:29:14 -0800

    services

--- public/include/ng/app/services.js
+++ public/include/ng/app/services.js
@@ -1,3 +1,11 @@
 'use strict';
 
 /* Services */
+
+var quoteServices = angular.module('quoteServices', ['ngResource']);
+
+quoteServices.factory('Quote', ['$resource',
+	function ($resource) {
+		return $resource('quotes/:source', {source: 'user'});
+	}
+]);
