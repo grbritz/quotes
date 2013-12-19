92a1de72f2b936cec7b6329797e1231b2e447a43
Alex Burner <alex.burner@gmail.com>
2013-12-19 13:21:51 -0800

    done ui fiddling

--- app/views/base.blade.php
+++ app/views/base.blade.php
@@ -46,7 +46,6 @@
 		<div class="modal-dialog">
 			<div class="modal-content">
 				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 					<h2 class="modal-title" id="newQuoteModalLabel">New quote</h2>
 				</div>
 				<div class="modal-body">
